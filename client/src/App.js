35e5f636c3befe8022dfb0938515cc55b6c520d8
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2024-07-27 20:00:55 +0530

    #4-create client psges-signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Signup from './component/Signup';
+
 function App() {
-  return <div className="App">hello word</div>;
+  return (
+    <Routes>
+      <Route path="/signup" exact element={<Signup />} />
+
+      <Route path="/" element={<Navigate replace to="/login" />} />
+    </Routes>
+  );
 }
 
 export default App;
