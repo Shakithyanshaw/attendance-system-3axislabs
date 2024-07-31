6264331a18f9e60ade5d3de141947fc6b9a343f1
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2024-07-31 07:25:31 +0530

    #4-client-sever-interaction-add validation for signin

--- client/src/component/Login/index.jsx
+++ client/src/component/Login/index.jsx
@@ -3,16 +3,40 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 
+const validations = {
+  email: {
+    regex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    message: 'Please enter a valid email address.',
+  },
+  password: {
+    regex:
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    message:
+      'Password must be at least 8 characters long, and include an uppercase letter, a lowercase letter, a number, and a special character.',
+  },
+};
+
 const Login = () => {
   const [data, setData] = useState({ email: '', password: '' });
   const [error, setError] = useState('');
 
   const handleChange = ({ currentTarget: input }) => {
     setData({ ...data, [input.name]: input.value });
+    if (!validations[input.name].regex.test(input.value)) {
+      setError(validations[input.name].message);
+    } else {
+      setError('');
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    for (let key in data) {
+      if (!validations[key].regex.test(data[key])) {
+        setError(validations[key].message);
+        return;
+      }
+    }
     try {
       const url = 'http://localhost:8080/api/auth';
       const { data: res } = await axios.post(url, data);
@@ -55,15 +79,15 @@ const Login = () => {
             />
             {error && <div className={styles.error_msg}>{error}</div>}
             <button type="submit" className={styles.green_btn}>
-              Sing In
+              Sign In
             </button>
           </form>
         </div>
         <div className={styles.right}>
-          <h1>New Student ?</h1>
+          <h1>New Student?</h1>
           <Link to="/signup">
             <button type="button" className={styles.white_btn}>
-              Sing Up
+              Sign Up
             </button>
           </Link>
         </div>
